198cf95b5adaed20c2e125630e0f1526189c205a
APTTUS\vbhan <vbhan@conga.com>
2023-09-29 14:27:28 +0530

    no message

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">Conga Digital Commerce June &#x27;23</a>
+                    <a href="index.html" data-type="index-link">Conga Digital Commerce October &#x27;23</a>
                 </li>
 
                 <li class="divider"></li>
